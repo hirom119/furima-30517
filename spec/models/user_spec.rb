792656766152478430cbddddd19b90f2ca18db41
hirom119 <71484414+hirom119@users.noreply.github.com>
2020-10-23 14:16:17 +0900

    ユーザー管理機能修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,7 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
+      
       it "passwordが空では登録できない"do
         @user.password = ''
         @user.valid?
@@ -43,18 +44,19 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it "passwordは６文字以上であれば登録できる"do
-        @user.password = "00000a"
-        expect(@user).to be_valid
+      it "passwordが半角英字のみの場合、登録できない"do
+        @user.password = "aaaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-        it "passwordは半角英数字であれば登録できる"do
-        @user.password ='00000a'
-        expect(@user).to be_valid
+        it "passwordは半角数字のみの場合、登録できない"do
+        @user.password ='111111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
         it "first_nameが空だと登録できない"do
         @user.first_name = ""
         @user.valid?
-
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it "first_name_kanaが空だと登録できない"do
