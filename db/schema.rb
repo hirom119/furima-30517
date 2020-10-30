c83a59d135a1323dd922ed3b3cf1c962b83acb61
hirom119 <71484414+hirom119@users.noreply.github.com>
2020-10-30 13:00:11 +0900

    active_storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_055421) do
+ActiveRecord::Schema.define(version: 2020_10_30_035835) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
